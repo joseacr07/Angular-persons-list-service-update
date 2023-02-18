0983c0be5fca01af5a05c5e4dd5a3c7a8ebb1135
xjoseacr07 <jcampos.equinox@gmail.com>
2023-02-18 15:03:18 +0100

    Event binding @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,20 @@ import { Persona } from './person.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Persons List';
-  persons: Persona[] = [ new Persona('Luis','Lopez'), new Persona('Laura','Gomez') ];
 
-  nombreInput:string = '';
-
-  apellidoInput:string = '';
-
-  agregarPersona(){
-    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
-    this.persons.push(persona1);
+  persons: Persona[] = [ 
+    new Persona('Luis','Lopez'), 
+    new Persona('Laura','Gomez') 
+  ];
+  
+  personaAgregada(persona:Persona){
+    this.persons.push(persona)
   }
-
+  
 }
+
+
+
+
