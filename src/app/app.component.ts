d4892c38be02fa2206a354c6649b4dca61d91dbc
xjoseacr07 <jcampos.equinox@gmail.com>
2023-02-18 13:28:41 +0100

    elo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './person.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Persons List';
+  persons: Persona[] = [ new Persona('Luis','Lopez'), new Persona('Laura','Gomez') ];
+
+  nombreInput:string = '';
+
+  apellidoInput:string = '';
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+    this.persons.push(persona1);
+  }
+
 }
